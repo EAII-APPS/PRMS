275773bc35c3a3e68ba73f175f8b66c42b220b9b
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2025-11-23 21:57:35 -0800

    Use document_user instead of request.user in generate_docx

--- backend/planApp/generate_docx.py
+++ backend/planApp/generate_docx.py
@@ -555,21 +555,22 @@ def generate_docx(request):
 
     if sector or division is not None:
         if sector is not None:
+            document_user=User.objects.filter(id=plandocument.added_by.id)
             table_data = generate_docx_data(
                 filter_year=filter_year,
                 filter_sector=sector,
                 filter_division=None,
                 filter_quarter=filter_quarter,
-                user=request.user,
+                user=document_user,
             )
         elif division is not None:
-            document_user=User.objects.filter(id=division)
+            document_user=User.objects.filter(id=plandocument.added_by.id)
             table_data = generate_docx_data(
                 filter_year=filter_year,
                 filter_sector=None,
                 filter_division=division,
                 filter_quarter=filter_quarter,
-                user=request.user,
+                user=document_user,
             )
     if (user.is_superuser or user.monitoring_id):
         document_user=User.objects.get(id=plandocument.added_by.id)
@@ -581,12 +582,13 @@ def generate_docx(request):
             user=document_user,
         )
     else:
+        document_user=User.objects.get(id=plandocument.added_by.id)
         table_data = generate_docx_data(
             filter_year=filter_year,
             filter_sector=sector,
             filter_division=division,
             filter_quarter=filter_quarter,
-            user=request.user,
+            user=document_user,
         )
     docx = Document()
     styles = docx.styles
